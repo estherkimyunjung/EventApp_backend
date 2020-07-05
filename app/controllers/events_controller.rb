3a9405b5abea339a9fef23fc3194bb9ed99ef6ca
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-04 21:14:23 -0500

    updated evets controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+    render :json(@events)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    render :json(@event)
+  end
+  
+  
 end
