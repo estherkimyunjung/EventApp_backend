49f3da90ce58bc97b87247e58d276d351b103e1f
lbirts <birtslauren@gmail.com>
2020-07-08 02:47:37 -0500

    functionalityDone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-skip_before_action :logged_in?, only: [:create, :index]
+skip_before_action :logged_in?, only: [:create]
 
     def index
         @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   resources :users
   resources :tickets
 
-  # get "/signup", to: "users#create"
-  # get "/login", to: "auth#create"
-  # post "/tickets", to: 'tickets#create'
   post "/login", to: "auth#create"
   post "/signup", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
